026cdf4b644a7fd4fc20c56a0ecb46aa8a5f54bf
BlackPearl <blackpearl@probotter.com>
2017-06-11 15:26:14 +0200

    Master - First Version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Python3-SimpleMySQL
+
+Usage:
+
+
+from classes import simpleMysql
+connect_mysql = simpleMysql.SimpleMysql('localhost', 'root', '', 'cms')
+
+test = connect_mysql.query("SELECT * FROM users WHERE id = %s" % (specSearch[0]["assetId"]))
+print(test)
+
+
+For more information look in the Code. It's very simple :)
---
+++ classes/simpleMysql.py
@@ -0,0 +1,74 @@
+import MySQLdb
+
+
+############################################################
+################## Simple MySQL ############################
+############################################################
+
+class SimpleMysql:
+    # Set Connection Details
+    __host = None
+    __user = None
+    __password = None
+    __db = None
+
+    # Temp Objects for Connection
+    __instance = None
+    __connection = None
+
+    def __init__(self, host='localhost', user='root', password=None, db=None):
+
+        self.__host = host
+        self.__user = user
+        self.__password = password
+        self.__db = db
+
+    def __open(self):
+        try:
+            con = MySQLdb.connect(self.__host, self.__user, self.__password, self.__db)
+            self.__connection = con
+            self.__session = con.cursor()
+        except MySQLdb.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
+
+    def __close(self):
+        self.__session.close()
+        self.__connection.close()
+
+    def query(self, query, commit=True):
+        mode = query.split(" ")[0].upper()
+
+        if (mode == "SELECT"):
+            self.__open()
+            self.__session.execute(query)
+            number_rows = self.__session.rowcount
+            number_columns = len(self.__session.description)
+
+            if number_rows >= 1 and number_columns > 1:
+                result = [item for item in self.__session.fetchall()]
+            else:
+                result = [item[0] for item in self.__session.fetchall()]
+            self.__close()
+
+            return result
+
+        elif mode == "DELETE":
+            self.__open()
+            self.__session.execute(query)
+            if commit == True:
+                self.__connection.commit()
+
+            delete_rows = self.__session.rowcount
+            self.__close()
+
+            return delete_rows
+
+        elif mode == "INSERT":
+            self.__open()
+            self.__session.execute(query)
+            if commit == True:
+                self.__connection.commit()
+
+            self.__close()
+
+            return self.__session.lastrowid
